19cf13b91170ae67f852b18987e75125b1ca5695
felix2feng <felix2feng@gmail.com>
2021-03-02 16:44:39 -0800

    Fix tests

--- test/protocol/setToken.spec.ts
+++ test/protocol/setToken.spec.ts
@@ -179,12 +179,10 @@ describe("SetToken", () => {
       firstComponentUnits = ether(1);
       secondComponent = await deployer.mocks.deployTokenMock(manager.address);
       secondComponentUnits = ether(2);
-      const thirdComponent = await deployer.mocks.deployTokenMock(manager.address);
-      const thirdComponentUnits = ether(3);
 
       controller = await deployer.core.deployController(owner.address);
-      components = [firstComponent.address, secondComponent.address, thirdComponent.address];
-      units = [firstComponentUnits, secondComponentUnits, thirdComponentUnits];
+      components = [firstComponent.address, secondComponent.address];
+      units = [firstComponentUnits, secondComponentUnits];
       modules = [mockBasicIssuanceModule.address, mockLockedModule.address];
       name = "TestSetToken";
       symbol = "SET";
@@ -511,7 +509,7 @@ describe("SetToken", () => {
         });
 
         it("should revert", async () => {
-          await expect(subject()).to.be.revertedWith("Must not be added module");
+          await expect(subject()).to.be.revertedWith("Module already added");
         });
       });
 
