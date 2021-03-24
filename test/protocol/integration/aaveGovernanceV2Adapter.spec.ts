bbdbc90cd29fb360cbf60979d87a115690663f10
Noah Citron <noah@jeff.org>
2021-03-24 13:08:49 -0400

    Add AaveGovernanceV2Adapter (#14)
    * add basic structure for SnapshotDelegationModule
    
    * add delegate function
    
    * add SnapshotGovernanceAdapter
    
    * code review changes
    
    * add aave v2 governance contracts to aave fixture
    
    * add AaveGovernanceV2Adapter
    
    * add tests for getting proper calldata
    
    * add integration test for aave governance v2 adapter
    
    * style fixes
    
    * code review changes

---
+++ test/protocol/integration/aaveGovernanceV2Adapter.spec.ts
@@ -0,0 +1,208 @@
+import "module-alias/register";
+import { BigNumber } from "@ethersproject/bignumber";
+import { defaultAbiCoder } from "@ethersproject/abi";
+import { Address, Bytes } from "@utils/types";
+import { Account } from "@utils/test/types";
+import { EMPTY_BYTES, ZERO } from "@utils/constants";
+import { AaveGovernanceV2Adapter } from "@utils/contracts";
+import DeployHelper from "@utils/deploys";
+import {
+  addSnapshotBeforeRestoreAfterEach,
+  getAccounts,
+  getWaffleExpect,
+  getAaveFixture,
+  getRandomAddress
+} from "@utils/test/index";
+import { AaveFixture } from "@utils/fixtures";
+
+
+const expect = getWaffleExpect();
+
+describe("AaveGovernanceAdapter", () => {
+  let owner: Account;
+  let deployer: DeployHelper;
+  let aaveGovernanceV2Adapter: AaveGovernanceV2Adapter;
+  let mockSetToken: Account;
+  let aaveSetup: AaveFixture;
+
+  before(async () => {
+    [
+      owner,
+      mockSetToken,
+    ] = await getAccounts();
+
+    deployer = new DeployHelper(owner.wallet);
+
+    aaveSetup = getAaveFixture(owner.address);
+    await aaveSetup.initialize();
+
+    aaveGovernanceV2Adapter = await deployer.adapters.deployAaveGovernanceV2Adapter(
+      aaveSetup.aaveGovernanceV2.address,
+      aaveSetup.aaveToken.address
+    );
+  });
+
+  addSnapshotBeforeRestoreAfterEach();
+
+  describe("#constructor", async () => {
+    let subjectAaveToken: Address;
+    let subjectAaveGovernance: Address;
+
+    beforeEach(async () => {
+      subjectAaveToken = aaveSetup.aaveToken.address;
+      subjectAaveGovernance = aaveSetup.aaveGovernanceV2.address;
+    });
+
+    async function subject(): Promise<any> {
+      return deployer.adapters.deployAaveGovernanceAdapter(
+        subjectAaveGovernance,
+        subjectAaveToken,
+      );
+    }
+
+    it("should have the correct AAVE token address", async () => {
+      const deployedAaveGovernanceAdapter = await subject();
+
+      const actualAaveToken = await deployedAaveGovernanceAdapter.aaveToken();
+      expect(actualAaveToken).to.eq(subjectAaveToken);
+    });
+
+    it("should have the correct Aave governance contract address", async () => {
+      const deployedAaveGovernanceAdapter = await subject();
+
+      const actualGovernanceAddress = await deployedAaveGovernanceAdapter.aaveProtoGovernance();
+      expect(actualGovernanceAddress).to.eq(subjectAaveGovernance);
+    });
+  });
+
+  describe("#getProposeCalldata", async () => {
+    let subjectProposalData: Address;
+
+    const targets = ["0xd08E12367A7D68CAA8ff080D3A56b2dc6650709b"];
+    const values = [0];
+    const signatures: string[] = [];
+    const calldatas = ["0x61461954"];
+    const withDelegateCall = [true];
+    const ipfsHash = "0x384dd57abcd23aae459877625228062db4082485a0ac1fc45eb54524f5836507";
+
+    beforeEach(async () => {
+      subjectProposalData = defaultAbiCoder.encode(
+        ["address", "address[]", "uint256[]", "string[]", "bytes[]", "bool[]", "bytes32"],
+        [aaveSetup.executor.address, targets, values, signatures, calldatas, withDelegateCall, ipfsHash]
+      );
+    });
+
+
+    async function subject(): Promise<any> {
+      return aaveGovernanceV2Adapter.getProposeCalldata(subjectProposalData);
+    }
+
+    it("should return the correct data for creating a proposal", async () => {
+        const [targetAddress, ethValue, callData] = await subject();
+
+        const expectedCalldata = aaveSetup.aaveGovernanceV2.interface.encodeFunctionData(
+          "create", [aaveSetup.executor.address, targets, values, signatures, calldatas, withDelegateCall, ipfsHash]
+        );
+
+        expect(targetAddress).to.eq(aaveSetup.aaveGovernanceV2.address);
+        expect(ethValue).to.eq(BigNumber.from(0));
+        expect(callData).to.eq(expectedCalldata);
+    });
+  });
+
+  describe("#getVoteCalldata", async () => {
+    let subjectProposalId: BigNumber;
+    let subjectSupport: boolean;
+    let subjectData: Bytes;
+
+    beforeEach(async () => {
+      subjectProposalId = ZERO;
+      subjectSupport = true;
+      subjectData = EMPTY_BYTES;
+    });
+
+    async function subject(): Promise<any> {
+      return aaveGovernanceV2Adapter.getVoteCalldata(subjectProposalId, subjectSupport, subjectData);
+    }
+
+    it("should return correct data for voting", async () => {
+      const [targetAddress, ethValue, callData] = await subject();
+      const expectedCallData = aaveSetup.aaveGovernanceV2.interface.encodeFunctionData(
+        "submitVote",
+        [subjectProposalId, subjectSupport]
+      );
+
+      expect(targetAddress).to.eq(aaveSetup.aaveGovernanceV2.address);
+      expect(ethValue).to.eq(ZERO);
+      expect(callData).to.eq(expectedCallData);
+    });
+
+    describe("when voting against a proposal", () => {
+
+      beforeEach(async () => {
+        subjectSupport = false;
+      });
+
+      it("should return correct data for voting", async () => {
+        const [targetAddress, ethValue, callData] = await subject();
+
+        const expectedCallData = aaveSetup.aaveGovernanceV2.interface.encodeFunctionData(
+          "submitVote",
+          [subjectProposalId, subjectSupport]
+        );
+
+        expect(targetAddress).to.eq(aaveSetup.aaveGovernanceV2.address);
+        expect(ethValue).to.eq(ZERO);
+        expect(callData).to.eq(expectedCallData);
+      });
+    });
+  });
+
+  describe("#getDelegateCalldata", async () => {
+    let subjectDelegatee: Address;
+
+    beforeEach(async () => {
+      subjectDelegatee = await getRandomAddress();
+    });
+
+    async function subject(): Promise<any> {
+      return aaveGovernanceV2Adapter.getDelegateCalldata(subjectDelegatee);
+    }
+
+    it("should return correct data for delegation", async () => {
+      const [targetAddress, ethValue, callData] = await subject();
+
+      const expectedCallData = aaveSetup.aaveToken.interface.encodeFunctionData("delegate", [subjectDelegatee]);
+
+      expect(targetAddress).to.eq(aaveSetup.aaveToken.address);
+      expect(ethValue).to.eq(ZERO);
+      expect(callData).to.eq(expectedCallData);
+    });
+  });
+
+  describe("#getRegisterCalldata", async () => {
+    let subjectSetToken: Address;
+
+    beforeEach(async () => {
+      subjectSetToken = mockSetToken.address;
+    });
+
+    async function subject(): Promise<any> {
+      return aaveGovernanceV2Adapter.getRegisterCalldata(subjectSetToken);
+    }
+
+    it("should revert", async () => {
+      await expect(subject()).to.be.revertedWith("No register available in AAVE governance");
+    });
+  });
+
+  describe("#getRevokeCalldata", async () => {
+    async function subject(): Promise<any> {
+      return aaveGovernanceV2Adapter.getRevokeCalldata();
+    }
+
+    it("should revert", async () => {
+      await expect(subject()).to.be.revertedWith("No revoke available in AAVE governance");
+    });
+  });
+});
