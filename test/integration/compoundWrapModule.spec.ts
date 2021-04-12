7338cc4ef600c84b0ca22da3304a041af4d3aa69
cgewecke <christophergewecke@gmail.com>
2021-04-12 11:32:52 -0700

    Use fully qualified lib name in CompoundWrapModule test

--- test/integration/compoundWrapModule.spec.ts
+++ test/integration/compoundWrapModule.spec.ts
@@ -23,7 +23,7 @@ import { CompoundFixture, SystemFixture } from "@utils/fixtures";
 
 const expect = getWaffleExpect();
 
-describe("compoundWrapModule", () => {
+describe.only("compoundWrapModule", () => {
   let owner: Account;
   let deployer: DeployHelper;
   let setup: SystemFixture;
@@ -70,7 +70,10 @@ describe("compoundWrapModule", () => {
 
     // compoundWrapAdapter setup
     const compoundLibrary = await deployer.libraries.deployCompound();
-    const compoundWrapAdapter = await deployer.adapters.deployCompoundWrapAdapter("Compound", compoundLibrary.address);
+    const compoundWrapAdapter = await deployer.adapters.deployCompoundWrapAdapter(
+      "contracts/protocol/integration/lib/Compound.sol:Compound",
+      compoundLibrary.address
+    );
     await setup.integrationRegistry.addIntegration(wrapModule.address, compoundWrapAdapterIntegrationName, compoundWrapAdapter.address);
   });
 
