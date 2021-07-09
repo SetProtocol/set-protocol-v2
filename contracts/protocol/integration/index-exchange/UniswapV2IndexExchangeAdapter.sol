afd26915bd72b35402c66859b59d9f4db1976798
ncitron <noah@jeff.org>
2021-07-08 22:08:05 -0400

    improve comments

--- contracts/protocol/integration/index-exchange/UniswapV2IndexExchangeAdapter.sol
+++ contracts/protocol/integration/index-exchange/UniswapV2IndexExchangeAdapter.sol
@@ -68,6 +68,7 @@ contract UniswapV2IndexExchangeAdapter is IIndexExchangeAdapter {
      * @param  _isSendTokenFixed         Boolean indicating if the send quantity is fixed, used to determine correct trade interface
      * @param  _sourceQuantity           Fixed/Max amount of source token to sell
      * @param  _destinationQuantity      Min/Fixed amount of destination token to buy
+     * @param  _data                     Encoded address intermediary token in the trade path. If empty, path is the input and output tokens
      *
      * @return address                   Target contract address
      * @return uint256                   Call value
