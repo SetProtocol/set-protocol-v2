27186f9fb95525aaacc20e7218e9edb9402e73c0
ncitron <noah@jeff.org>
2021-08-13 21:10:10 -0400

    review changes

--- contracts/protocol/integration/wrap-v2/AaveV2WrapV2Adapter.sol
+++ contracts/protocol/integration/wrap-v2/AaveV2WrapV2Adapter.sol
@@ -1,14 +1,18 @@
 /*
     Copyright 2021 Set Labs Inc.
+
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
+
     SPDX-License-Identifier: Apache License, Version 2.0
 */
 
@@ -38,7 +42,8 @@ contract AaveV2WrapV2Adapter {
 
     /* ========== State Variables ========= */
 
-    // Address of the AaveV2Forwarder helper contract
+    // Address of the Aave LendingPool contract
+    // Note: this address may change in the event of an upgrade
     ILendingPool public lendingPool;
 
     /* ============ Constructor ============ */
