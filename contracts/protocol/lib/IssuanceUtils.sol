17b059d444f95d24ed3479f3428c3fff33caf716
alpha-guy <alphaguy2019@gmail.com>
2021-08-19 13:30:51 +0530

    Moved validation logic to IssuanceUtils library

---
+++ contracts/protocol/lib/IssuanceUtils.sol
@@ -0,0 +1,62 @@
+/*
+    Copyright 2020 Set Labs Inc.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    SPDX-License-Identifier: Apache License, Version 2.0
+*/
+
+pragma solidity 0.6.10;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeCast } from "@openzeppelin/contracts/utils/SafeCast.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+
+import { ISetToken } from "../../interfaces/ISetToken.sol";
+import { PreciseUnitMath } from "../../lib/PreciseUnitMath.sol";
+
+
+/**
+ * @title IssuanceUtils
+ * @author Set Protocol
+ *
+ * A collection of utility functions to help during issuance/redemption of SetToken.
+ */
+library IssuanceUtils {
+    using SafeMath for uint256;
+    using SafeCast for int256;
+    using PreciseUnitMath for uint256;
+
+    /**
+     * Validates the component token transfer to/from SetToken during issuance/redemption. Reverts if Set is undercollateralized post transfer.
+     *
+     * @param _setToken         Instance of the SetToken being issued/redeemed
+     * @param _component        Address of component being transferred in/out
+     * @param _isIssue          True if issuing SetToken, false if redeeming
+     * @param _quantity         Total SetToken quantity with fees being issued. Pass 0 if Set is being redeemed.
+     */
+    function validateComponentTransfer(ISetToken _setToken, address _component, bool _isIssue, uint256 _mintQuantity) internal {
+        
+        uint256 newComponentBalance = IERC20(_component).balanceOf(address(_setToken));    
+
+        uint256 positionUnit = _setToken.getDefaultPositionRealUnit(address(_component)).toUint256();   
+        uint256 newTotalSupply = _isIssue
+            ? _setToken.totalSupply().add(_mintQuantity)    // Mint happens after this function is called
+            : _setToken.totalSupply();                  // Burn takes place before this function is called
+        
+        require(
+            newComponentBalance >= newTotalSupply.preciseMul(positionUnit),
+            "Invalid transfer. Results in undercollateralization"
+        );
+    }
+}
--- contracts/protocol/modules/AaveDebtIssuanceModule.sol
+++ contracts/protocol/modules/AaveDebtIssuanceModule.sol
@@ -20,28 +20,26 @@ pragma solidity 0.6.10;
 pragma experimental "ABIEncoderV2";
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { SafeCast } from "@openzeppelin/contracts/utils/SafeCast.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 
 import { DebtIssuanceModule } from "./DebtIssuanceModule.sol";
 import { IController } from "../../interfaces/IController.sol";
 import { Invoke } from "../lib/Invoke.sol";
 import { ISetToken } from "../../interfaces/ISetToken.sol";
-import { PreciseUnitMath } from "../../lib/PreciseUnitMath.sol";
-
+import { IssuanceUtils } from "../../lib/IssuanceUtils.sol";
 
 /**
- * @title AaveDebtIssuanceModule
+ * @title DebtIssuanceModuleV2
  * @author Set Protocol
  *
- * The AaveDebtIssuanceModule is a module that enables users to issue and redeem SetTokens that contain default and all
+ * The DebtIssuanceModuleV2 is a module that enables users to issue and redeem SetTokens that contain default and all
  * external positions, including debt positions. Module hooks are added to allow for syncing of positions, and component
  * level hooks are added to ensure positions are replicated correctly. The manager can define arbitrary issuance logic
  * in the manager hook, as well as specify issue and redeem fees.
- * Note: This module is designed to issue/redeem SetTokens which hold one or more aTokens as components.
+ * 
+ * NOTE: This module supports issuing/redeeming SetTokens which hold one or more aTokens as components.
  */
-contract AaveDebtIssuanceModule is DebtIssuanceModule {
+contract DebtIssuanceModuleV2 is DebtIssuanceModule {
     
     /* ============ Constructor ============ */
     
@@ -50,11 +48,14 @@ contract AaveDebtIssuanceModule is DebtIssuanceModule {
     /* ============ Internal Functions ============ */
     
     /**
-     * NOTE: Overrides DebtIssuanceModule#_resolveEquityPositions internal function.
      * Resolve equity positions associated with SetToken. On issuance, the total equity position for an asset (including default and external
      * positions) is transferred in. Then any external position hooks are called to transfer the external positions to their necessary place.
      * On redemption all external positions are recalled by the external position hook, then those position plus any default position are
      * transferred back to the _to address.
+     *
+     * NOTE: Overrides DebtIssuanceModule#_resolveEquityPositions internal function and adds the undercollateralization checks in place of 
+     * the default strict token balances checks. The undercollateralization checks implemented in IssuanceUtils#validateComponentTransfer revert
+     * upon undercollateralization of the SetToken post transfer.
      */
     function _resolveEquityPositions(
         ISetToken _setToken,
@@ -78,7 +79,7 @@ contract AaveDebtIssuanceModule is DebtIssuanceModule {
                         address(_setToken),
                         componentQuantity
                     );
-                    _validateATokenTransfer(_setToken, component, _quantity, _isIssue);
+                    IssuanceUtils.validateComponentTransfer(_setToken, component, _isIssue, _quantity);
 
                     _executeExternalPositionHooks(_setToken, _quantity, IERC20(component), true, true);
                 } else {
@@ -86,32 +87,9 @@ contract AaveDebtIssuanceModule is DebtIssuanceModule {
 
                     Invoke.invokeTransfer(_setToken, component, _to, componentQuantity);
 
-                    _validateATokenTransfer(_setToken, component, _quantity, _isIssue);
+                    IssuanceUtils.validateComponentTransfer(_setToken, component, _isIssue, 0);
                 }
             }
         }
-    }
-
-    /**
-     * Validates the AToken transfer to/from SetToken during issuance/redemption. Reverts if Set is undercollateralized post transfer.
-     *
-     * @param _setToken         Instance of the SetToken being issued/redeemed
-     * @param _component        Address of component being transferred in/out
-     * @param _quantity         Total SetToken quantity with/net fees being issued/redeemed
-     * @param _isIssue          True if issuing SetToken, false if redeeming
-     */
-    function _validateATokenTransfer(ISetToken _setToken, address _component, uint256 _quantity, bool _isIssue) internal {
-        
-        uint256 newComponentBalance = IERC20(_component).balanceOf(address(_setToken));    
-
-        uint256 positionUnit = _setToken.getDefaultPositionRealUnit(address(_component)).toUint256();   
-        uint256 newTotalSupply = _isIssue
-            ? _setToken.totalSupply().add(_quantity)    // Mint happens after this function is called
-            : _setToken.totalSupply();      // Burn takes place before this function is called
-        
-        require(
-            newComponentBalance >= newTotalSupply.preciseMul(positionUnit),
-            "Invalid transfer. Results in undercollateralization"
-        );
     }
 }
