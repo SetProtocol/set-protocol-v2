eef3f7e502130115ed670c4d18d5d015cefc81f0
cgewecke <christophergewecke@gmail.com>
2021-09-07 08:21:00 -0700

    Remove gas-fixing subtask for TASK_TEST_SETUP_TEST_ENVIRONMENT

--- tasks/subtasks.ts
+++ tasks/subtasks.ts
@@ -22,26 +22,4 @@ subtask(TASK_COMPILE_SOLIDITY_GET_ARTIFACT_FROM_COMPILATION_OUTPUT)
   }
 );
 
-// Fix gas to be string instead of number in typechain files
-internalTask(TASK_TEST_SETUP_TEST_ENVIRONMENT)
-  .setAction(async ({ input }, { config }, runSuper) => {
-    if (process.env.NO_COMPILE === "true") return;
-
-    const replace = require("replace-in-file");
-
-    const options = {
-      files: [ "./typechain/**/*.ts" ],
-      from: /gas\: [0-9]+,/g,
-      to: (match: string) => match.replace(/gas: ([0-9]+),/g, 'gas: "$1",'),
-    };
-
-    try {
-      replace.sync(options);
-      console.log("Fixing gas cost type from number to string...");
-    } catch (error) {
-      console.error("Error occurred:", error);
-    }
-});
-
-
 export {};
