fbc2106d038ffb7b1546aa92699858c481a74153
ncitron <noah@jeff.org>
2021-09-10 22:01:01 -0400

    add BalancerV2IndexExchangeAdapter

---
+++ contracts/interfaces/external/IBVault.sol
@@ -0,0 +1,30 @@
+pragma solidity 0.6.10;
+pragma experimental ABIEncoderV2;
+
+interface IBVault {
+    
+    enum SwapKind { GIVEN_IN, GIVEN_OUT }
+
+    function swap(
+        SingleSwap memory singleSwap,
+        FundManagement memory funds,
+        uint256 limit,
+        uint256 deadline
+    ) external payable returns (uint256);
+
+    struct SingleSwap {
+        bytes32 poolId;
+        SwapKind kind;
+        address assetIn;
+        address assetOut;
+        uint256 amount;
+        bytes userData;
+    }
+
+    struct FundManagement {
+        address sender;
+        bool fromInternalBalance;
+        address payable recipient;
+        bool toInternalBalance;
+    }
+}
