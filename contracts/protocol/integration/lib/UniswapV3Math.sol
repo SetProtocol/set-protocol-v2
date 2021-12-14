8015d0348f5f436f9a1ddc1b3e3d93d363f1cf0f
cgewecke <christophergewecke@gmail.com>
2021-12-13 17:25:42 -0800

    Add param natspec to UniswapV3Math methods

--- contracts/protocol/integration/lib/UniswapV3Math.sol
+++ contracts/protocol/integration/lib/UniswapV3Math.sol
@@ -34,7 +34,13 @@ library UniswapV3Math {
 
     /**
      * @dev Converts a UniswapV3 sqrtPriceX96 value to a priceX96 value. This method is borrowed from
-     * PerpProtocol's `lushan` repo, in lib/PerpMath
+     * PerpProtocol's `lushan` repo, in lib/PerpMath.
+     *
+     * For more info about the sqrtPriceX96 format see:
+     * https://docs.uniswap.org/sdk/guides/fetching-prices#understanding-sqrtprice
+     *
+     * @param _sqrtPriceX96     Square root of a UniswapV3 encoded fixed-point pool price.
+     * @return                  _sqrtPriceX96 converted to a priceX96 value
      */
     function formatSqrtPriceX96ToPriceX96(uint160 _sqrtPriceX96) internal pure returns (uint256) {
         return FullMath.mulDiv(_sqrtPriceX96, _sqrtPriceX96, FixedPoint96.Q96);
@@ -43,6 +49,12 @@ library UniswapV3Math {
     /**
      * @dev Converts a UniswapV3 X96 format price into a PRECISE_UNIT price. This method is borrowed from
      * PerpProtocol's `lushan` repo, in lib/PerpMath
+     *
+     * For more info about the priceX96 format see:
+     * https://docs.uniswap.org/sdk/guides/fetching-prices#understanding-sqrtprice
+     *
+     * @param _valueX96         UniswapV3 encoded fixed-point pool price
+     * @return                  _priceX96 as a PRECISE_UNIT value
      */
     function formatX96ToX10_18(uint256 _valueX96) internal pure returns (uint256) {
         return FullMath.mulDiv(_valueX96, PreciseUnitMath.preciseUnit(), FixedPoint96.Q96);
