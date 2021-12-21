9a9c5b835bd6eb1fab556ef40993eccc2989ad23
Richard Guan <guan.richard1@gmail.com>
2021-12-22 01:46:05 +1100

    fix but not quite

--- contracts/protocol/modules/WrapModuleV2.sol
+++ contracts/protocol/modules/WrapModuleV2.sol
@@ -33,7 +33,6 @@ import { IWrapV2Adapter } from "../../interfaces/IWrapV2Adapter.sol";
 import { ModuleBase } from "../lib/ModuleBase.sol";
 import { Position } from "../lib/Position.sol";
 import { PreciseUnitMath } from "../../lib/PreciseUnitMath.sol";
-import "hardhat/console.sol";
 
 /**
  * @title WrapModuleV2
@@ -461,9 +460,6 @@ contract WrapModuleV2 is ModuleBase, ReentrancyGuard {
             address(_setToken),
             _wrapData
         );
-
-        console.log(callTarget);
-        console.log(callValue);
 
         _setToken.invoke(callTarget, callValue, callByteData);
     }
