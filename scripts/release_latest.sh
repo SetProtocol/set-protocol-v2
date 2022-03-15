2d1c7b1311a4ddb96f2f1e73841618b6a7a26ffa
cgewecke <christophergewecke@gmail.com>
2022-03-15 11:07:01 -0700

    Try to fix missing package version update

--- scripts/release_latest.sh
+++ scripts/release_latest.sh
@@ -6,6 +6,12 @@ set -o errexit
 echo '{
   "branches": [
     { "name": "master" }
+  ],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/git"
   ]
 }' > .releaserc.json
 
