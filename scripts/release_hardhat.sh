958e67033f61b416cd3938c9e9931ff764216ef6
cgewecke <christophergewecke@gmail.com>
2022-03-15 14:41:07 -0700

    fix(test): debug (#212)

--- scripts/release_hardhat.sh
+++ scripts/release_hardhat.sh
@@ -10,7 +10,7 @@ set -o errexit
 echo '{
   "branches": [
     { "name": "release_default_do_not_delete" },
-    { "name": "chris/test-semantic-release", "channel": "hardhat", "prerelease": "hh"}
+    { "name": "chris/test-semantic-release", "channel": "hardhat", "prerelease": "hhh"}
   ]
 }' > .releaserc.json
 
