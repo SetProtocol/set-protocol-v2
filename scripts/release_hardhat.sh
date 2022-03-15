5d8df213610c7ef7854d19830e92f0923e83a95b
cgewecke <christophergewecke@gmail.com>
2022-03-15 14:09:18 -0700

    fix(test) test again (#210)

---
+++ scripts/release_hardhat.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+# Auto-publishes a specially built release to the @hardhat tag using version schema `x.x.x-hardhat.1`
+# See scripts/prepublish_only.sh for details about this build.
+#
+# The `release_default` branch config here is necessary to trick the semantic-release tool into publishing
+# latest and hardhat builds from `master`. These are handled by separate jobs in CI (see circleci/config.yml)
+echo '{
+  "branches": [
+    { "name": "release_default_do_not_delete" },
+    { "name": "chris/test-semantic-release", "channel": "hardhat", "prerelease": "hh"}
+  ]
+}' > .releaserc.json
+
+PUBLISH_HARDHAT=true npx semantic-release --debug
