0143a47b19fdab479e2f2c485c51827faeaf5ddd
Christian Koopmann <christian@MTMAC0057.local>
2022-04-21 19:42:29 +0800

    Add skeleton integration test for NotionalTradeModule

---
+++ test/integration/notionalTradeModule.spec.ts
@@ -0,0 +1,139 @@
+import "module-alias/register";
+
+import { BigNumber } from "ethers";
+
+import { Account, ForkedTokens } from "@utils/test/types";
+import DeployHelper from "@utils/deploys";
+import { ether } from "@utils/index";
+import {
+  cacheBeforeEach,
+  getAccounts,
+  getForkedTokens,
+  getRandomAccount,
+  getSystemFixture,
+  getWaffleExpect,
+  initializeForkedTokens,
+} from "@utils/test/index";
+
+import {  SystemFixture } from "@utils/fixtures";
+import {
+  SetToken,
+  DebtIssuanceModuleV2,
+  ManagerIssuanceHookMock,
+} from "@utils/contracts";
+
+import { IERC20 } from "@typechain/IERC20";
+import { MAX_UINT_256 } from "@utils/constants";
+
+const expect = getWaffleExpect();
+
+/**
+ * Tests the icETH rebalance flow.
+ *
+ * The icETH product is a composite product composed of:
+ * 1. stETH
+ * 2. WETH
+ */
+describe("Notional trade module integration [ @forked-mainnet ]", () => {
+  let owner: Account;
+  let manager: Account;
+
+  let deployer: DeployHelper;
+
+
+  let setup: SystemFixture;
+  let tokens: ForkedTokens;
+
+  let weth: IERC20;
+  let steth: IERC20;
+  let debtIssuanceModule: DebtIssuanceModuleV2;
+  let mockPreIssuanceHook: ManagerIssuanceHookMock;
+
+  let setToken: SetToken;
+  let issueQuantity: BigNumber;
+
+  cacheBeforeEach(async () => {
+    [owner, manager] = await getAccounts();
+
+    deployer = new DeployHelper(owner.wallet);
+
+    setup = getSystemFixture(owner.address);
+    await setup.initialize();
+
+    // Setup ForkedTokens
+    await initializeForkedTokens(deployer);
+    tokens = getForkedTokens();
+    weth = tokens.weth;
+    steth = tokens.steth;
+
+
+    // Deploy DebtIssuanceModuleV2
+    debtIssuanceModule = await deployer.modules.deployDebtIssuanceModuleV2(
+      setup.controller.address
+    );
+
+    await setup.controller.addModule(debtIssuanceModule.address);
+
+    // Deploy mock issuance hook to pass as arg in DebtIssuance module initialization
+    mockPreIssuanceHook = await deployer.mocks.deployManagerIssuanceHookMock();
+
+    // Create liquidity
+    const ape = await getRandomAccount();   // The wallet adding initial liquidity
+    await weth.transfer(ape.address, ether(50));
+    await steth.transfer(ape.address, ether(50000));
+
+  });
+
+  async function initialize() {
+    // Create Set token
+    setToken = await setup.createSetToken(
+      [steth.address],
+      [ether(2)],
+      [debtIssuanceModule.address],
+      manager.address,
+    );
+
+    // Fund owner with stETH
+    await tokens.steth.transfer(owner.address, ether(11000));
+
+    // stETH has balance rounding errors that crash DebtIssuanceModuleV2 with:
+    //  "Invalid transfer in. Results in undercollateralization"
+    // > transfer quantity =              1000000000000000000
+    // > stETH balanceOf after transfer =  999999999999999999
+    // Transfer steth to set token to overcollaterize the position by exactly the rounding error
+    // Transferring 2 results in steth.balanceOf(setToken) == 1
+    await tokens.steth.transfer(setToken.address, 2);
+
+    await steth
+      .connect(owner.wallet)
+      .approve(debtIssuanceModule.address, MAX_UINT_256);
+
+
+    // Initialize debIssuance module
+    await debtIssuanceModule.connect(manager.wallet).initialize(
+      setToken.address,
+      ether(.1),
+      ether(0), // No issue fee
+      ether(0), // No redeem fee
+      owner.address,
+      mockPreIssuanceHook.address
+    );
+
+
+    // Issue
+    issueQuantity = ether(1);
+
+    await debtIssuanceModule
+      .connect(owner.wallet)
+      .issue(setToken.address, issueQuantity, owner.address);
+  }
+
+  describe("#trade", async () => {
+    cacheBeforeEach(initialize);
+    it("should work", async () => {
+      const setTokenBalance = await setToken.balanceOf(owner.address);
+      expect(setTokenBalance).to.eq(issueQuantity);
+    });
+  });
+
+});
