aed6f346e220d889632234d96d20b16734fcc74b
Christian Koopmann <christian@MTMAC0057.local>
2022-05-04 09:17:48 +0800

    Remove unneeded contracts

--- contracts/protocol/integration/wrap/notional/WrappedfCash.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.11;
-import { WrappedfCash as WrappedfCashBase } from "notional-solidity-sdk/contracts/fCash/WrappedfCash.sol";
-import { NotionalProxy } from "notional-solidity-sdk/interfaces/notional/NotionalProxy.sol";
-
-contract WrappedfCash is WrappedfCashBase {
-    constructor(NotionalProxy _notionalProxy) WrappedfCashBase(_notionalProxy){
-    }
-}
