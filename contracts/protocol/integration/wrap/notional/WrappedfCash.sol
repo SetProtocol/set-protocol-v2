75670c12c8a6a9443816a56d5c4fcdfa7e570ce0
cgewecke <christophergewecke@gmail.com>
2022-05-25 12:10:29 -0700

    Add noop public state variable to WrappedfCash, nUpgradeableBeacon for hardhat-etherscan

--- contracts/protocol/integration/wrap/notional/WrappedfCash.sol
+++ contracts/protocol/integration/wrap/notional/WrappedfCash.sol
@@ -5,6 +5,9 @@ import { INotionalV2 } from "wrapped-fcash/interfaces/notional/INotionalV2.sol";
 import { IWETH9 } from "wrapped-fcash/interfaces/IWETH9.sol";
 
 contract WrappedfCash is wfCashERC4626 {
+    // Does nothing - disambiguates this contract from WrappedfCashFactory so
+    // hardhat-etherscan can verify it.
+    uint8 public noop;
     constructor(INotionalV2 _notionalProxy, IWETH9 _weth) wfCashERC4626(_notionalProxy, _weth){
     }
 }
--- contracts/protocol/integration/wrap/notional/nUpgradeableBeacon.sol
+++ contracts/protocol/integration/wrap/notional/nUpgradeableBeacon.sol
@@ -6,6 +6,9 @@ import "openzeppelin-contracts-V4/proxy/beacon/UpgradeableBeacon.sol";
 /// @dev Re-exporting to make available to brownie
 /// UpgradeableBeacon is Ownable, default owner is the deployer
 contract nUpgradeableBeacon is UpgradeableBeacon {
+    // Does nothing - disambiguates this contract from WrappedfCashFactory so
+    // hardhat-etherscan can verify it.
+    uint8 public noop;
     constructor(address implementation_) UpgradeableBeacon(implementation_) {}
 }
 
