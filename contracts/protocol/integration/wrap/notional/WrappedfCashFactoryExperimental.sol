756a3024321d68ce34fdb9891dd5e079be075003
cgewecke <christophergewecke@gmail.com>
2022-05-25 09:34:59 -0700

    Add noop state variable to WrappedfCashFactoryExperimental to introduce bytecode difference

--- contracts/protocol/integration/wrap/notional/WrappedfCashFactoryExperimental.sol
+++ contracts/protocol/integration/wrap/notional/WrappedfCashFactoryExperimental.sol
@@ -2,7 +2,12 @@
 pragma solidity 0.8.11;
 import { WrappedfCashFactory } from "wrapped-fcash/contracts/proxy/WrappedfCashFactory.sol";
 
+// This contract will be removed before NotionalTradeModule goes to production. It's temporarily included
+// to support module development and staging_mainnet testing before Notional deploys their own version of it.
 contract WrappedfCashFactoryExperimental is WrappedfCashFactory {
+    // Does nothing - disambiguates this contract from WrappedfCashFactory so
+    // hardhat-etherscan can verify it.
+    uint8 public noop;
     constructor(address _beacon) WrappedfCashFactory(_beacon){
     }
 }
