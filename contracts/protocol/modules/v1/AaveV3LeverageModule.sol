34268ae1e4c94743e5065a2235ee191c073095a9
ckoopmann <c.k.e.koopmann@gmail.com>
2023-04-05 12:59:06 +0800

    Fix wrong Copyright headers

--- contracts/protocol/modules/v1/AaveV3LeverageModule.sol
+++ contracts/protocol/modules/v1/AaveV3LeverageModule.sol
@@ -1,5 +1,5 @@
 /*
-    Copyright 2022 Index Coop
+    Copyright 2023 Index Coop
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
